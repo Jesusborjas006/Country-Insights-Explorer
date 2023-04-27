98c382bd4d3ca0e1956f89be73710cf2d255017c
Jesus Borjas <jesusbor006@gmail.com>
2023-04-27 14:13:22 -0500

    Add navbar component

---
+++ src/Components/Navbar/Navbar.js
@@ -0,0 +1,12 @@
+import "./Navbar.css";
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <h1>Country Insights Explorer</h1>
+      <p>Dark Mode</p>
+    </nav>
+  );
+};
+
+export default Navbar;
