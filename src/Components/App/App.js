0fbce04e95113ddfd1a380cd47ac115336d8d918
Jesus Borjas <jesusbor006@gmail.com>
2023-04-28 17:57:20 -0500

    Add basic route functionality to display new page

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -3,6 +3,7 @@ import Navbar from "../Navbar/Navbar";
 import "./App.css";
 import getCountries from "../../apiCalls";
 import CountriesContainer from "../CountriesContainer/CountriesContainer";
+import { Link, Route, Switch } from "react-router-dom";
 
 function App() {
   const [countryData, setCountryData] = useState([]);
@@ -22,15 +23,24 @@ function App() {
     <div>
       <Navbar />
       <form className="form">
+        <Link to="/country/1">New Page</Link>
         <input
-        className="search-input"
+          className="search-input"
           type="text"
           value={query}
           onChange={(event) => setQuery(event.target.value)}
           placeholder="Search for a country..."
         />
       </form>
-      <CountriesContainer allCountries={filteredCountries} />
+      <Switch>
+        <Route
+          path="/"
+          exact
+          render={() => <CountriesContainer allCountries={filteredCountries} />}
+        />
+
+        <Route path="/country/1" render={() => <h2>Country Details</h2>} />
+      </Switch>
     </div>
   );
 }
